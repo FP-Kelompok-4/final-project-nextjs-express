e9c9a481c9bf5470ccbdf8e88752ee2666ab604d
vyaninsyanurmuhammad <vyaninsya.nurmuhammad@gmail.com>
2024-06-03 17:18:45 +0700

    feat(FK4-13): add footer components

--- apps/web/src/components/Link-Brand.tsx
+++ apps/web/src/components/Link-Brand.tsx
@@ -1,15 +1,32 @@
-import { Vegan } from "lucide-react";
-import Link from 'next/link';
+import { cn } from '@/lib/utils';
+import { Vegan } from 'lucide-react';
+import Link, { LinkProps } from 'next/link';
 import React from 'react';
 
-const LinkBrand = () => {
+interface LinkBrandProps
+  extends Omit<React.AnchorHTMLAttributes<HTMLAnchorElement>, keyof LinkProps> {
+  href?: string;
+  isBigLink?: boolean;
+}
+
+const LinkBrand = ({ className, href = '/', isBigLink }: LinkBrandProps) => {
   return (
     <Link
-      href={'/'}
-      className="text-gossamer-500 inline-flex items-center gap-1"
+      href={href}
+      className={cn(
+        'text-gossamer-500 inline-flex items-center',
+        isBigLink ? 'gap-3' : 'gap-1',
+
+        className,
+      )}
     >
-      <Vegan strokeWidth={4} />
-      <span className="text-xl font-extrabold tracking-tighter">
+      <Vegan strokeWidth={4} size={isBigLink ? 32 : 24} />
+      <span
+        className={cn(
+          'font-extrabold tracking-tighter',
+          isBigLink ? 'text-3xl' : 'text-xl',
+        )}
+      >
         StayCation
       </span>
     </Link>
