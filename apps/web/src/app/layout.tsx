5cc0f5ccf54a7b83a4561f5388f2792e1ed1dc36
vyaninsyanurmuhammad <vyaninsya.nurmuhammad@gmail.com>
2024-06-04 16:27:50 +0700

    refactor(FK4-13): :art: change structure layout to group

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -26,11 +26,7 @@ export default function RootLayout({
           inter.variable,
         )}
       >
-        <ReduxProvider>
-          <Header className="fixed top-0 z-50 border-b-[1px] bg-white" />
-          {children}
-          <Footer />
-        </ReduxProvider>
+        <ReduxProvider>{children}</ReduxProvider>
       </body>
     </html>
   );
