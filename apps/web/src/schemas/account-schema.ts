040b48de1158d1c58d27c1b667bdb71ba5bcfcf4
vyaninsyanurmuhammad <vyaninsya.nurmuhammad@gmail.com>
2024-06-09 14:22:18 +0700

    feat(FK4-28): add account schema

---
+++ apps/web/src/schemas/account-schema.ts
@@ -0,0 +1,29 @@
+import * as z from 'zod';
+
+export const AccountSchema = z
+  .object({
+    email: z
+      .string()
+      .email({
+        message: 'Invalid email address',
+      })
+      .trim(),
+    name: z
+      .string()
+      .min(3, {
+        message: 'Must be 3 or more characters long',
+      })
+      .trim(),
+    birthdate: z.date({
+      required_error: 'A date of birth is required.',
+    }),
+    gender: z
+      .string()
+      .min(1, {
+        message: 'Must be 1 characters long',
+      })
+      .max(1, {
+        message: 'Must be 1 characters long',
+      }),
+  })
+  .partial();
