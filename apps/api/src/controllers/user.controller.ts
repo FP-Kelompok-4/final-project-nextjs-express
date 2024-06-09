602520622018af9464a74fa71f6d258dd757d72f
vyaninsyanurmuhammad <vyaninsya.nurmuhammad@gmail.com>
2024-06-09 14:15:35 +0700

    feat(FK4-28): :alien: add putAccountUser controller

--- apps/api/src/controllers/user.controller.ts
+++ apps/api/src/controllers/user.controller.ts
@@ -1,6 +1,6 @@
 import { UserService } from '@/services/user.service';
 import { NextFunction, Request, Response } from 'express';
-import { AddUserReq } from 'models/user.model';
+import { AddUserReq, UpdateAccountUserReq } from 'models/user.model';
 
 export class UserController {
   async postUser (req: Request, res: Response, next: NextFunction) {
@@ -8,6 +8,20 @@ export class UserController {
       const request = req.body as AddUserReq;
 
       const user = await UserService.addUser(request);
+
+      res.status(201).send({
+        data: user
+      })
+    } catch (e) {
+      next(e)
+    }
+  }
+
+  async putAccountUser (req: Request, res: Response, next: NextFunction) {
+    try {
+      const request = req.body as UpdateAccountUserReq;
+
+      const user = await UserService.updateAccountUser(request);
 
       res.status(201).send({
         data: user
