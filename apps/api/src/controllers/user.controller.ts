0198151f31da8cba0a55eaece7ea5663787900dc
vyaninsyanurmuhammad <vyaninsya.nurmuhammad@gmail.com>
2024-06-09 15:35:04 +0700

    refactor(FK4-28): :alien: implement id param and formating code

--- apps/api/src/controllers/user.controller.ts
+++ apps/api/src/controllers/user.controller.ts
@@ -3,31 +3,33 @@ import { NextFunction, Request, Response } from 'express';
 import { AddUserReq, UpdateAccountUserReq } from 'models/user.model';
 
 export class UserController {
-  async postUser (req: Request, res: Response, next: NextFunction) {
+  async postUser(req: Request, res: Response, next: NextFunction) {
     try {
       const request = req.body as AddUserReq;
 
       const user = await UserService.addUser(request);
 
       res.status(201).send({
-        data: user
-      })
+        data: user,
+      });
     } catch (e) {
-      next(e)
+      next(e);
     }
   }
 
-  async putAccountUser (req: Request, res: Response, next: NextFunction) {
+  async putAccountUser(req: Request, res: Response, next: NextFunction) {
     try {
+      const { id } = req.params;
+
       const request = req.body as UpdateAccountUserReq;
 
-      const user = await UserService.updateAccountUser(request);
+      const user = await UserService.updateAccountUser(id, request);
 
       res.status(201).send({
-        data: user
-      })
+        data: user,
+      });
     } catch (e) {
-      next(e)
+      next(e);
     }
   }
 }
