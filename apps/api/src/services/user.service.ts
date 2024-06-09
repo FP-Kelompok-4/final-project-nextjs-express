9753a2baa4f339716fa419b4ac7db8d419161b37
vyaninsyanurmuhammad <vyaninsya.nurmuhammad@gmail.com>
2024-06-09 14:17:59 +0700

    feat(FK4-28): :alien: add updateAccountUser service

--- apps/api/src/services/user.service.ts
+++ apps/api/src/services/user.service.ts
@@ -1,26 +1,44 @@
 import { ResponseError } from '@/error/response-error';
 import prisma from '@/prisma';
 import bcrypt from 'bcrypt';
-import { AddUserReq, toAddUserRes } from 'models/user.model';
+import {
+  AddUserReq,
+  toAddUserRes,
+  UpdateAccountUserReq,
+  toUpdateAccountUserRes,
+} from 'models/user.model';
 
 export class UserService {
   static async addUser(req: AddUserReq) {
     const existingEmail = await prisma.user.findUnique({
       where: {
-        email: req.email
-      }
-    })
+        email: req.email,
+      },
+    });
 
     if (existingEmail) {
       throw new ResponseError(400, 'Email already exist!');
     }
 
-    (req.password) ? req.password = await bcrypt.hash(req.password, 10) : req.password = null;
+    req.password
+      ? (req.password = await bcrypt.hash(req.password, 10))
+      : (req.password = null);
 
     const user = await prisma.user.create({
-      data: req
-    })
+      data: req,
+    });
 
     return toAddUserRes(user);
+  }
+
+  static async updateAccountUser(req: UpdateAccountUserReq) {
+    const user = await prisma.user.update({
+      where: {
+        email: req.email,
+      },
+      data: { name: req.name, gender: req.gender, birthdate: req.birthdate },
+    });
+
+    return toUpdateAccountUserRes(user);
   }
 }
