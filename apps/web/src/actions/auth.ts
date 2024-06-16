284a2fb7dbb4ffd8f78ae181a6b113e6012c44ce
vyaninsyanurmuhammad <vyaninsya.nurmuhammad@gmail.com>
2024-06-16 16:09:58 +0700

    feat(FK4-20): add disable change password for google provider

--- apps/web/src/actions/auth.ts
+++ apps/web/src/actions/auth.ts
@@ -1,45 +1,45 @@
-'use server'
+'use server';
 
-import * as z from 'zod'
-import { AuthError, CredentialsSignin } from "next-auth"
-import { signIn, signOut } from "@/auth"
-import { SigninSchema, SignupSchema } from '@/schemas/auth-schema'
-import { api } from '@/lib/axios'
-import axios, { AxiosError } from 'axios'
-import { DEFAULT_LOGIN_REDIRECT_AS_USER } from "@/routes"
+import * as z from 'zod';
+import { AuthError, CredentialsSignin } from 'next-auth';
+import { signIn, signOut } from '@/auth';
+import { SigninSchema, SignupSchema } from '@/schemas/auth-schema';
+import { api } from '@/lib/axios';
+import axios, { AxiosError } from 'axios';
+import { DEFAULT_LOGIN_REDIRECT_AS_USER } from '@/routes';
 
 export const singup = async (values: z.infer<typeof SignupSchema>) => {
   try {
-    await api.post('users/', values)
+    await api.post('users/', { ...values, provider: 'credential' });
 
-    return { success: 'Success signup!' }
+    return { success: 'Success signup!' };
   } catch (e) {
-    if(axios.isAxiosError(e)) {
+    if (axios.isAxiosError(e)) {
       return {
-        error: e.response?.data.message
-      }
+        error: e.response?.data.message,
+      };
     }
   }
-}
+};
 
 export const signin = async (values: z.infer<typeof SigninSchema>) => {
   try {
-    await signIn("credentials", {
+    await signIn('credentials', {
       ...values,
-      redirectTo: DEFAULT_LOGIN_REDIRECT_AS_USER
-    })
+      redirectTo: DEFAULT_LOGIN_REDIRECT_AS_USER,
+    });
   } catch (e) {
-    if(e instanceof AuthError) {
+    if (e instanceof AuthError) {
       if (e.cause?.err instanceof AxiosError) {
-        return { error: e.cause.err.response?.data.message};
+        return { error: e.cause.err.response?.data.message };
       }
-      
-      return { error: 'Something went wrong!'};
+
+      return { error: 'Something went wrong!' };
     }
     throw e;
   }
-}
+};
 
 export const signout = async () => {
-  await signOut({ redirectTo: '/', redirect:true });
-}
+  await signOut({ redirectTo: '/', redirect: true });
+};
