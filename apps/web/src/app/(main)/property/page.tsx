d23b5d00d6c5a141c602aa9ae34043c584ee76b8
yousenurf27 <yosenurf@gmail.com>
2024-06-28 22:19:46 +0700

    style(FK4-51): :bug: handle error when data undefined in page property

--- apps/web/src/app/(main)/property/page.tsx
+++ apps/web/src/app/(main)/property/page.tsx
@@ -31,35 +31,43 @@ const Property = () => {
         </div>
       ):(
         <>
-          <div className="mb-12 px-6 md:px-10 xl:px-20">
-            <h1 className="my-6 text-sm font-semibold">Found {formatNumberEn(1000)} places</h1>
-            <div className="grid md:grid-cols-4 gap-x-6 gap-y-10">
-              {properties.map((d) => (
-                <PropertyCard key={d.id} data={d} />
-              ))}
+          {properties ? (
+            <>
+              <div className="mb-12 px-6 md:px-10 xl:px-20">
+                <h1 className="my-6 text-sm font-semibold">Found {formatNumberEn(1000)} places</h1>
+                <div className="grid md:grid-cols-4 gap-x-6 gap-y-10">
+                  {properties.map((d) => (
+                    <PropertyCard key={d.id} data={d} />
+                  ))}
+                </div>
+              </div>
+              <div className="mb-6">
+                <Pagination>
+                  <PaginationContent>
+                    <PaginationItem>
+                      <PaginationPrevious href="#" />
+                    </PaginationItem>
+                    <PaginationItem>
+                      <PaginationLink href="#" isActive>1</PaginationLink>
+                    </PaginationItem>
+                    <PaginationItem>
+                      <PaginationLink href="#">2</PaginationLink>
+                    </PaginationItem>
+                    <PaginationItem>
+                      <PaginationEllipsis />
+                    </PaginationItem>
+                    <PaginationItem>
+                      <PaginationNext href="#" />
+                    </PaginationItem>
+                  </PaginationContent>
+                </Pagination>
+              </div>
+            </>
+          ):(
+            <div className="min-h-[calc(100svh-79px)] w-full flex justify-center items-center">
+              <h2 className="text-xl font-semibold">Opps. Property not found.</h2>
             </div>
-          </div>
-          <div className="mb-6">
-            <Pagination>
-              <PaginationContent>
-                <PaginationItem>
-                  <PaginationPrevious href="#" />
-                </PaginationItem>
-                <PaginationItem>
-                  <PaginationLink href="#" isActive>1</PaginationLink>
-                </PaginationItem>
-                <PaginationItem>
-                  <PaginationLink href="#">2</PaginationLink>
-                </PaginationItem>
-                <PaginationItem>
-                  <PaginationEllipsis />
-                </PaginationItem>
-                <PaginationItem>
-                  <PaginationNext href="#" />
-                </PaginationItem>
-              </PaginationContent>
-            </Pagination>
-          </div>
+          )}
         </>
       )}
     </main>
